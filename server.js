de2d59c50badbb265d20cf722b305daed625378b
Marc Petitdemange <marc.petitdemange57@gmail.com>
2021-11-01 22:08:19 +0100

    Exercice-7

--- server.js
+++ server.js
@@ -1,27 +1,27 @@
-import * as http from 'http'; //Import du module http syntaxe ES6
+import express from 'express'; //Import du module express
 import * as logger from './moduleDeLog.js' //Import de notre module de log syntaxe ES6
 
-var server = http.createServer(function (request, response) {
+var app = express();
 
-    let page = request.url;
-    console.log(page);
-
-    response.writeHead(200, {"Content-Type" : "text/plain; charset=utf-8"});
-
-    if(page === "/"){
-        response.write("Merci de te présenter au choixpeau jeune sorcier !");
-        logger.info("Tu t'es présenté au choixpeau ! Je suis fier de toi ! "); //On utilise notre info logger écrit précédemment
-    }
-    else if(page === "/Griffondor"){
-        response.write("Hmm je sens du courage ! Griffondor !");
-        logger.debug("Mais quel courage !"); //On utilise notre debug logger écrit précédemment
-    }else if(page === "/Griffondor/etudiant/1/content"){
-        response.write("Les informations du premier élève de Griffondor");
-        logger.error("C'est très formel tout ça !"); //On utilise notre error logger écrit précédemment
-    } else{
-        response.write("Tu t'es perdu dans le chemin de travers Harry !");
-        logger.hagridLogger(page) //Hagrid va nous dire que l'on s'est perdu ! On utilise notre hagridLogger !
-    }
-    response.end();
+app.get('/',function (req, res, next) { //On gère l'url par défaut
+    res.send("Merci de te présenter au choixpeau jeune sorcier !");
+    logger.info("Tu t'es présenté au choixpeau ! Je suis fier de toi ! ");
 });
-server.listen(9000);
+
+app.get("/Griffondor", function (req, res, next) { //On gère l'url /Griffondor
+    res.send("Hmm je sens du courage ! Griffondor !");
+    logger.debug("Mais quel courage !");
+});
+
+app.get("/Griffondor/etudiant/:id/content", function (req, res, next) { //On peut facilement récupérer l'id ici
+    res.send("Les informations de l'élève numéro : " + req.params.id + " de Griffondor");
+    logger.hagridLogger("Tu cherches à afficher les informations de l'élève numéro : " + req.params.id + " Harry !");
+});
+
+app.use(function(req, res, next) { //On gère les autres cas
+    let response = res.status(404);
+    response.send("Tu t'es perdu dans le chemin de travers Harry !");
+    logger.hagridLogger("Je ne connais pas le chemin suivant : " + req.url);
+});
+
+app.listen(9000);
